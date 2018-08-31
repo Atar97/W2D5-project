b447c4b92e86587cc7651c82b4792fd91bd887df
austin_cotant <austin.cotant@gmail.com>
2018-08-31 13:55:06 -0700

    Finish int set

---
+++ LRU/lib/p01_int_set.rb
@@ -0,0 +1,98 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max)
+  end
+
+  def insert(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    return false if num < 0
+    num < @store.length
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+     self[num] << num unless include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % @store.length]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet < IntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    @count += 1 unless self.include?(num)
+    if count == num_buckets
+      resize!
+    end  
+    
+    super
+    
+  end
+  # 
+  def remove(num)
+    @count -= 1 if self.include?(num)
+    super 
+  end
+
+  private
+
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    temp = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        temp[el % temp.length] << el
+      end 
+    end 
+    @store = temp
+  end
+end
