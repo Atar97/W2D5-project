24c9a9d72ee8292aa524b0d0fc558a99a6d39dc1
austin_cotant <austin.cotant@gmail.com>
2018-08-31 14:41:58 -0700

    Functional hash set

--- LRU/lib/p02_hashing.rb
+++ LRU/lib/p02_hashing.rb
@@ -1,14 +1,28 @@
+require 'json'
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
+  
   def hash
+    val = self[0].object_id
+    (1...self.length).each do |el|
+      val ^= (self[el].object_id * el)
+      
+    end 
+    val.hash 
   end
+  
 end
 
 class String
   def hash
+    val = self[0].ord
+    (1...self.length).each do |el|
+      val ^= (self[el].ord * el)
+    end 
+    val.hash  
   end
 end
 
@@ -16,6 +30,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_json.hash
   end
+  
 end
--- LRU/lib/p03_hash_set.rb
+++ LRU/lib/p03_hash_set.rb
@@ -1,3 +1,6 @@
+require_relative 'p01_int_set'
+
+
 class HashSet
   attr_reader :count
 
@@ -7,18 +10,28 @@ class HashSet
   end
 
   def insert(key)
+    unless self[key].include?(key)
+      @count += 1 
+      if @count == num_buckets
+        resize! 
+      end 
+      self[key] << key
+    end 
   end
 
   def include?(key)
+    self[key].include?(key)
+    
   end
 
   def remove(key)
+    @count -= 1 if self[key].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +39,12 @@ class HashSet
   end
 
   def resize!
+    temp = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        temp[el % temp.length] << el
+      end 
+    end 
+    @store = temp
   end
 end
